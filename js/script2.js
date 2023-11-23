131a207b0f01cb1a91ef3aa5423cfd35d50a3e7d
Daniel Navarrete Cuevas <danielnavarrete2304@gmail.com>
2023-11-22 18:26:58 -0600

    Added toggle of Reviews

--- js/script2.js
+++ js/script2.js
@@ -5,3 +5,12 @@ toggles.forEach(toggle => {
         toggle.parentNode.classList.toggle('active');
     });
 });
+
+function toggleWord(wordId) {
+    var word = document.getElementById(wordId);
+    if (word.classList.contains('hidden')) {
+        word.classList.remove('hidden');
+    } else {
+        word.classList.add('hidden');
+    }
+}
